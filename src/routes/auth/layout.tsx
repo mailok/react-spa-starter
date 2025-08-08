c6534de3448387c435c0d6decfdf9acecfc5c3dd
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-08-08 12:51:44 -0300

    feat(auth): enhance signup page with google integration and form validation

--- src/routes/auth/layout.tsx
+++ src/routes/auth/layout.tsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router";
 export function AuthLayout() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 w-full">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="flex w-full max-w-sm flex-col gap-6 items-center">
         <Link
           to="/"
           className="flex items-center gap-2 self-center font-medium"
--- src/routes/auth/signup/signup.page.tsx
+++ src/routes/auth/signup/signup.page.tsx
@@ -1,78 +1,120 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { GoogleLogo } from "@/components/google-log";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Link } from "react-router";
 import { PasswordInput } from "@/components/ui/password-input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
 
 export function SignupPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
   return (
-    <div className="mx-auto w-full max-w-sm sm:max-w-md flex flex-col gap-6">
-      <Card className="w-full">
+    <div className="flex flex-col gap-6 w-full">
+      <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Create an account</CardTitle>
+          <CardDescription>Sign up with your Google account</CardDescription>
+          <Button className="mt-3 w-full gap-3" variant="outline">
+            <GoogleLogo />
+            Continue with Google
+          </Button>
         </CardHeader>
-
         <CardContent>
-          <form>
-            <div className="grid gap-6">  
-              {/* Fields */}
-              <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="name">Full name</Label>
-                  <Input
-                    id="name"
-                    name="name"
-                    placeholder="Jane Doe"
-                    required
-                    autoFocus
-                  />
-                </div>
+          <div className="my-2 w-full flex items-center justify-center overflow-hidden">
+            <Separator />
+            <span className="text-sm px-2">OR</span>
+            <Separator />
+          </div>
 
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="lorem@example.com"
-                    required
-                  />
-                </div>
-
-                <div className="grid gap-3">
-                  <Label htmlFor="password">Password</Label>
-                  <PasswordInput id="password" name="password" required />
-                </div>
-
-                <div className="grid gap-3">
-                  <Label htmlFor="confirm">Confirm password</Label>
-                  <PasswordInput id="confirm" name="confirm" required />
-                </div>
-
-                <Button type="submit" className="w-full">
-                  Create account
-                </Button>
-              </div>
-
-              {/* Switch to login */}
-              <div className="text-center text-sm">
-                Already have an account?{" "}
-                <Link to="/auth/login" className="underline underline-offset-4">
-                  Log in
-                </Link>
-              </div>
-            </div>
-          </form>
+          <Form {...form}>
+            <form
+              className="w-full space-y-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <PasswordInput
+                        placeholder="Password"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="mt-4 w-full">
+                Continue with Email
+              </Button>
+            </form>
+          </Form>
         </CardContent>
+        <p className="mt-5 text-sm text-center">
+          Already have an account?
+          <Link
+            to="/auth/login"
+            className="ml-1 underline text-muted-foreground"
+          >
+            Log in
+          </Link>
+        </p>
       </Card>
-
-      {/* Legal */}
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our{" "}
-        <Link to="/terms-of-service">Terms of Service</Link> and{" "}
-        <Link to="/privacy-policy">Privacy Policy</Link>.
-      </div>
     </div>
   );
 }
