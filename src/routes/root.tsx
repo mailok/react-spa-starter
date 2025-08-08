8a3712d83c6ee6e58685c2687cf45c774eca11ea
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-08-08 17:01:07 -0300

    feat(routes): update Root component with new header layout and GitHub link button

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,22 +1,40 @@
 import { Outlet } from "react-router";
 import { AppSidebar } from "../components/app-sidebar";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
-import { Separator } from "@radix-ui/react-separator";
 import { Breadcrumbs } from "./breadcrums";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export function Root() {
   return (
     <>
-      <AppSidebar />
+      <AppSidebar variant="inset" />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
             <SidebarTrigger className="-ml-1" />
             <Separator
               orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
+              className="mx-2 data-[orientation=vertical]:h-4"
             />
             <Breadcrumbs />
+            <div className="ml-auto flex items-center gap-2">
+              <Button
+                variant="ghost"
+                asChild
+                size="sm"
+                className="hidden sm:flex"
+              >
+                <a
+                  href="https://github.com/mailok/react-spa-starter"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="dark:text-foreground"
+                >
+                  GitHub
+                </a>
+              </Button>
+            </div>
           </div>
         </header>
         <div className="size-full">
